65ed705e9375cd551d08e888f2ac7f8d85f13251
Zhiqiao Xu <xuzhiqiao97@gmail.com>
2016-12-03 14:54:32 +0800

    Create 5-3.c

---
+++ chapter5/5-3.c
@@ -0,0 +1,15 @@
+main()
+{
+	char x[20] = "azzzz\0";
+	char y[6] = "bzzzz\0";
+	istrcat(x, y);
+	printf("x = %c \n ", *x);
+}
+
+void istrcat(char *x, char *y)
+{
+	while (*x) 
+		x++;
+	while (*x++ = *y++) 
+		;
+}
